be20f13b0a7cdc38913db9bf4b0ec867738130c4
khadimul723 <mkikhadimul1112@gmail.com>
2023-11-29 20:00:39 +0600

    demo application

--- lib/main.dart
+++ lib/main.dart
@@ -115,6 +115,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
+      // this is floating action button
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
